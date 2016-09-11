db5dd1b248e8e0a88d5d8477e6a6d237a01d2ee0
morrna <morrna@gmail.com>
2016-09-11 14:20:33 -0600

    ran a spider; got no output

---
+++ spider_coloradoan.py
@@ -0,0 +1,9 @@
+import scrapy
+
+class ColoradoanSpider(scrapy.Spider):
+	name = 'Coloradoan'
+	start_urls = ['http://www.coloradoan.com/news']
+
+	def parse(self, response):
+		for href in response.css('.hgpm-link a::attr(href)'):
+			yield href
