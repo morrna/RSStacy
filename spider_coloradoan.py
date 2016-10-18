bf571092a2aa367a293782f7ede148e38440f7ca
Nathan Morrison <morrna@gmail.com>
2016-10-18 02:18:59 -0600

    Attempting to use CrawlSpider on the Coloradoan.

--- spider_coloradoan.py
+++ spider_coloradoan.py
@@ -1,10 +1,14 @@
-import scrapy
+import scrapy as scr
+import scrapy.spiders as spi
+import scrapy.linkextractors as lx
 
-class ColoradoanSpider(scrapy.Spider):
-	name = 'Coloradoan'
-	start_urls = ['http://www.coloradoan.com/news']
+class ColoradoanSpider(spi.CrawlSpider):
+        name = 'Coloradoan'
+        allowed_domains = ['coloradoan.com']
+        start_urls = ['http://www.coloradoan.com/news']
+        
+        rules = [ spi.Rule(lx.LinkExtractor(allow='story/news'))
+                ]
 
-	def parse(self, response):
-		trial_class = 'hgpm-link'
-		for thing in response.css(trial_class):
-			yield { trial_class: thing.extract() }
+        def parse_item(self, response):
+            scr.inspect_response(response, self)
