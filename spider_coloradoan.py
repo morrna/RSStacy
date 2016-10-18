c20aac0a8f24eee172e346479175898214f5236a
Nathan Morrison <morrna@gmail.com>
2016-10-18 04:11:47 -0600

    fixing whitespace after editing with wrong vim config

--- spider_coloradoan.py
+++ spider_coloradoan.py
@@ -3,12 +3,12 @@ import scrapy.spiders as spi
 import scrapy.linkextractors as lx
 
 class ColoradoanSpider(scr.Spider):
-        name = 'Coloradoan'
-        allowed_domains = ['coloradoan.com']
-        start_urls = ['http://www.coloradoan.com/news']
-        
-        good_links = lx.LinkExtractor(allow='story/news')
+    name = 'Coloradoan'
+    allowed_domains = ['coloradoan.com']
+    start_urls = ['http://www.coloradoan.com/news']
+    
+    good_links = lx.LinkExtractor(allow='story/news')
 
-        def parse(self, response):
-            return dict(enumerate([ [link.url, link.text] \
-                    for link in self.good_links.extract_links(response) ] ))
+    def parse(self, response):
+        return dict(enumerate([ [link.url, link.text] \
+                               for link in self.good_links.extract_links(response) ] ))
