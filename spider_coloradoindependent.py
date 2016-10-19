009e19b71239708667d9a331faeda79d66737005
morrna <morrna@gmail.com>
2016-10-18 21:11:04 -0600

    incomplete, but some progress made toward extracting articles

--- spider_coloradoindependent.py
+++ spider_coloradoindependent.py
@@ -1,6 +1,7 @@
 import scrapy as scr
 import scrapy.spiders as spi
 import scrapy.linkextractors as lx
+import scrapy.shell as scs
 
 class COIndependentSpider(scr.Spider):
     name = 'ColoradoIndependent'
@@ -9,5 +10,18 @@ class COIndependentSpider(scr.Spider):
     start_urls = ['http://www.coloradoindependent.com/feed']
 
     def parse(self, response):
-        scr.inspect_response(response, self)
+
+        scs.inspect_response(response, self)
+        titles = response.xpath('//channel/item/title/text()')
+        links = response.xpath('//channel/item/link/text()')
+
+        for link in links:
+            yield scr.Request(link.extract(), callback=self.parse_page)
+
+    def parse_page(self, response):
+        scs.inspect_response(response, self)
+
+        pars = [p.extract() for p in response.xpath('//p[not(@class) and not(@style)]')]
+
+
 
